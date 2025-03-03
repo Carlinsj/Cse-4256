18aaa6caf8c1f8ab2cd70f0f224f73021ba6a03b
Carlinsj <carlinsjoseph17@gmail.com>
2025-03-03 11:20:59 -0500

    7

---
+++ Assignment_7.py
@@ -0,0 +1,11 @@
+def directed_to_undirected(di):
+    n = len(di)
+    undirected = [[0] * n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if di[i][j] == 1 or di[j][i] == 1:
+                undirected[i][j] = 1
+                undirected[j][i] = 1  # Ensure symmetry
+    
+    return undirected
